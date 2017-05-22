e29e14b5a5272db46805d5b943cc443c82cf955d
mattography <mattography@gmail.com>
2017-05-21 21:11:25 -0400

    adding project files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# angular-spotify-search
-Spotify search app with playlist creation and JSON export
+# Spotify Search with Angular
+Spotify search app with playlist creation and JSON export. This app uses the Spotify API to query for songs based on your search. It will return results which can be added to a playlist. Each song in your playlist can then have a special note, as well as URL to an image attached to it. Once you're ready, you can export your playlist to JSON.
+
+To use, follow the steps below:
+
+1. Clone the repo to wherever you'd like to run the application from.
+2. In terminal, cd to the projects directory and run `npm install`
+3. Once the required node packages have been downloaded, simply run `webpack-dev-server`, which will run the site from `localhost:8080`.
+
---
+++ js/entry.js
@@ -0,0 +1,33 @@
+angular.module('myApp', []).controller('MusicController', function($scope, $http) {
+  $scope.$watch('search', function() {
+    fetch();
+  });
+
+  $scope.search = "";
+  $scope.listLimit = "10";
+  $scope.selectedSongs = [];
+  $scope.addItem = function(song) {
+    $scope.selectedSongs.push(song);
+  }
+  $scope.remove = function(song) {
+    _.remove($scope.selectedSongs, function(val) {
+      return val === song;
+    });
+
+    $scope.selectedSongs.splice(index, 1);
+  };
+  $scope.IsVisible = false;
+  $scope.ShowHide = function () {
+      $scope.IsVisible = $scope.IsVisible ? false : true;
+  }
+
+  function fetch() {
+    $http.get("https://api.spotify.com/v1/search?q=" + $scope.search + "&type=track&limit=50")
+      .then(function(response) {
+        console.log(response.data.tracks.items);
+        $scope.isTheDataLoaded = true;
+        $scope.details = response.data.tracks.items;
+      });
+  }
+
+});
